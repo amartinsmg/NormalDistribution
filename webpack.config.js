6625b09274a8f1bd850f49bb0518ce22a813ecfe
amartinsmg <andremindurimg@gmail.com>
2022-09-24 08:57:47 -0300

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const CssMinimizerWebpack = require("css-minimizer-webpack-plugin"),
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+  path = require("path"),
+  TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    filename: "assets/bundle.js",
+    path: path.resolve(__dirname, "build"),
+    assetModuleFilename: 'assets/[hash][ext]',
+  },
+  mode: "production",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "build"),
+    },
+    port: 8080,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        // use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s(a|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ['html-loader']
+      }
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".css"],
+  },
+  optimization: {
+    minimizer: [new TerserPlugin(), new CssMinimizerWebpack()],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/bundle.css",
+    }),
+  ],
+};
