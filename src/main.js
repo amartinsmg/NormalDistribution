b80e8cd56e0b8eddaaa8dd41516acf66c6830c2d
amartinsmg <andremindurimg@gmail.com>
2022-09-25 00:18:39 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 require("./main.scss");
 const { drawChart } = require("./chart");
+const { gaussianCDF, gaussianPDF, zScore } = require("./gaussian");
 
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart);
