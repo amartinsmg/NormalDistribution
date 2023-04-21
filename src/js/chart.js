7f7b5b28987c4b0f832372a30803d7d68a8eb5b9
andre <andremindurimg@gmail.com>
2023-04-20 21:35:59 -0300

    Update chart.js

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,18 +1,38 @@
 const { stdGaussianPDF } = require("./gaussian");
 
+/**
+  Creates an array of data points to be used in a chart
+    @param xMin - minimum value of x-axis
+    @param xMax - maximum value of x-axis
+    @param mean - mean value of the Gaussian distribution
+    @param stdDev - standard deviation of the Gaussian distribution
+    @param x - the value of x for which the data point should be highlighted
+    @returns - an array of data points to be used in a chart
+*/
+
 function createArray(xMin, xMax, mean, stdDev, x) {
   const DataArr = [];
   let i = 0;
   for (let j = xMin; j <= xMax; j += 0.01 * stdDev) {
-    DataArr[i] = new Array(4);
-    DataArr[i][0] = j;
-    DataArr[i][1] = stdGaussianPDF(mean, stdDev, j);
-    DataArr[i][2] = j > x ? false : true;
-    DataArr[i][3] = "opacity: 1; + stroke-color: #000";
+    DataArr[i] = [
+      j,
+      stdGaussianPDF(mean, stdDev, j),
+      j > x ? false : true,
+      "opacity: 1; + stroke-color: #000",
+    ];
     i++;
   }
   return DataArr;
 }
+
+/**
+  Draws a chart using Google Charts API
+    @param xMin - minimum value of x-axis
+    @param xMax - maximum value of x-axis
+    @param mean - mean value of the Gaussian distribution
+    @param stdDev - standard deviation of the Gaussian distribution
+    @param x - the value of x for which the data point should be highlighted
+*/
 
 function drawChart(
   xMin = -5.1,
